b6b268a89b9d23d0d1f7a44619c9e59863179ca9
antoniomrrds <arqueiroverd@gmail.com>
2024-05-23 21:27:22 -0300

    add dependency injection mapper and others

--- TodoListApi/Program.cs
+++ TodoListApi/Program.cs
@@ -1,22 +1,20 @@
-using FluentValidation.AspNetCore;
-using TodoListApi.Application.Mappings;
+using System.Reflection;
+using FluentValidation;
 using TodoListApi.Application.Ports.Repositories;
-using TodoListApi.Application.Validators;
 using TodoListApi.Infrastructure.database;
 using TodoListApi.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<ITodoRepository, TodoRepository>();
-builder.Services.AddAutoMapper(typeof(TodoProfile));
-builder.Services.AddFluentValidation(fv => 
-    fv.RegisterValidatorsFromAssemblyContaining<CreateTodoDtoValidator>());builder.Services.AddSingleton(servicesProvider =>
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+
+builder.Services.AddSingleton(servicesProvider =>
 {
     var configuration = servicesProvider.GetRequiredService<IConfiguration>();
     var connectionString = configuration.GetConnectionString("DefaultConnection")
